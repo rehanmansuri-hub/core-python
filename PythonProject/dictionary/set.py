98d293ed94b1d2f83805d3c869a492baed6eae72
unknown <you@example.com>
2026-03-31 18:13:29 +0530

    'done'

---
+++ PythonProject/dictionary/set.py
@@ -0,0 +1,25 @@
+s = {'a','b','c,''d',}
+print(s)
+
+s = {'apple','mango','fruit','apple'}
+print(s)
+
+s = {10,'rehan',5.5}
+print(s)
+
+s = {'apple'}
+s.add('rehan')
+print(s)
+
+s = {'apple','mango'}
+s.remove('apple')
+print(s)
+
+
+s = {'monday','tuesday','wednesday'}
+s.discard('tuesday')
+print(s)
+
+s = {'rehan','nitin'}
+s.discard('nitin')
+print(s)
