622f4c553da05c05618f012aab7f1e6dad3af952
unknown <you@example.com>
2026-04-01 15:45:35 +0530

    done

---
+++ PythonProject/module/testuser.servise.py
@@ -0,0 +1,5 @@
+import userservise
+
+
+userservise.Add("Rehan","mansuri")
+userservise.Update("rehan", "mansuri")
---
+++ PythonProject/module/userservise.py
@@ -0,0 +1,11 @@
+
+def Add(firstName, lastName):
+    print("Adding User")
+    print("Name", firstName, lastName)
+    return
+
+
+def Update(firstName, lastName):
+    print("Update User")
+    print("Name", firstName, lastName)
+    return
